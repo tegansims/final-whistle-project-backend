d7a84f43b469f123e16a9eb0345902d15a5c12f4
tegansims <github email address>
2019-10-30 17:53:50 +0000

    password_digest set up for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password 
+  
   belongs_to :team
   belongs_to :usertype
   has_many :notes
   has_many :votes
   has_many :games, through: :team
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :email, uniqueness: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password_digest, :admin?, :player
+  attributes :id, :email, :admin?, :player
   has_one :team
   has_one :usertype
+  has_many :notes
+  has_many :votes
+  # has_many :games, through: :team
 end
