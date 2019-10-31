608ba985f7edda7d86b8376efed81ea0000f17cc
tegansims <github email address>
2019-10-31 18:53:21 +0000

    Auth: end of vid 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,23 @@ class UsersController < ApplicationController
     end
 
     def login
-        
         user = User.find_by(email: params[:email])
         
         if user && user.authenticate(params[:password])
-            render json: user
+            render json: { email: user.email, token: issue_token({ id: user.id }) }
         else
             render json: { error: 'Uh oh! Those details are invalid' }, status: 401
         end
     end
+
+    def validate
+        user = get_current_user
+        if user
+            render json: { email: user.email, token: issue_token({ id: user.id })  }
+        else
+            render json: {error: 'Invalid Token'}, status: 401
+        end
+    end
+
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :sports
 
   post '/login' , to: 'users#login'
+  get '/validate', to: 'users#validate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
