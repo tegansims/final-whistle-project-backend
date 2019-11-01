6f82ec0c755a9085e7989189dae4dbfa724dbc55
tegansims <github email address>
2019-11-01 14:40:30 +0000

    create team functionality done

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,20 @@
 class TeamsController < ApplicationController
 
+    def index
+        render json: Team.all 
+    end
+    
     def show
         render json: Team.find(params[:id])
     end
 
+    def create
+        # byebug
+        team = Team.create(team_params)
+    end
+   
+    def team_params
+        params.require(:team).permit(:name, :password, :password_confirmation, :sport_id)
+       
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   post '/login' , to: 'users#login'
   get '/validate', to: 'users#validate'
   post '/signup', to: 'users#create'
+  post '/createteam', to: 'teams#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
