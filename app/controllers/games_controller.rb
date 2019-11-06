3f4e2aef67429c3e9d33a5f6a5155cc7552ff606
tegansims <github email address>
2019-11-06 10:50:05 +0000

    admin ability to toggle complete/edit a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,17 +22,24 @@ class GamesController < ApplicationController
         game = Game.find(params['game']['game_id'])
         home = params['game']['homeScore']
         away = params['game']['awayScore']
-        category = Category.find(params['game']['category_id'])
+        category_id = params['game']['category_id']
+        completed = params['game']['completed']
         # byebug
 
         if home && away
             game.score = home+'-'+away
         end
 
-        if category
-            if category.id == 1
+        if completed == false
+            game.completed = false 
+        elsif completed == true
+            game.completed = true
+        end
+
+        if category_id
+            if Category.find(category_id) == 1
                 game.mom_winner = game.count_and_sort_votes(1)
-            elsif category.id == 2
+            elsif Category.find(category_id) == 2
                 game.dod_winner = game.count_and_sort_votes(2)
             end
         end
