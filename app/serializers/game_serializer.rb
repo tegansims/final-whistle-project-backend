4fa09c600a6982b31cbb998f02966ef3f30c8a2d
tegansims <github email address>
2019-11-13 09:31:33 +0000

    removed :votes from game, :board from team srlzrs

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -2,7 +2,7 @@ class GameSerializer < ActiveModel::Serializer
   attributes :id, :date, :venue, :opposition, :completed, :score, :mom_winner, :dod_winner
   has_one :team
   
-  has_many :votes
+  # has_many :votes
   has_many :notes
   # has_many :users, through: :team
   # has_many :players, through: :team
--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,5 +1,5 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :id, :name, :colour1, :colour2
   has_one :sport
-  has_many :boards
+  # has_many :boards
 end
